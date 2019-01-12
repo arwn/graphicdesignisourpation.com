7e70bbf5a8bfe4cdfa9c82a56dad3de93338a671
arwn <windham.aren@protonmail.com>
2019-01-12 21:44:33 +0000

    initial

---
+++ cart.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>memes go here</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/store.css" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<div class="navbar">
+        <ul>
+	<li><a href="index.php"><i class="fa fa-home"></i></a></li>
+	<li><a href="store.php">Store</a></li>
+        <li class="current"><a href="cart.php">Cart</a></li>
+        <li><a href="help.php">Help</a></li>
+	</ul>
+</div>
+<div class="shop">shop here</div>
+</body>
+</html>
---
+++ store.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>memes go here</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles/store.css" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<div class="navbar">
+        <ul>
+	<li><a href="index.php"><i class="fa fa-home"></i></a></li>
+	<li class="current"><a href="store.php">Store</a></li>
+        <li><a href="cart.php">Cart</a></li>
+        <li><a href="help.php">Help</a></li>
+	</ul>
+</div>
+<div class="store">
+	<h1>PURCHASE OUR CHEAP SERVICE!</h1>
+	<p>Nonrefundable.</p>
+	<div class="item">This is a store item</div>
+	<div class="item">This is a store item</div>
+	<div class="item">This is a store item</div>
+	<div class="item">This is a store item</div>
+</div>
+</body>
+</html>
